b5a16a77bbf387f83ab571ec3612ceb7e9929d52
Kaime <kamchadx@gmail.com>
2023-04-11 09:59:52 -0300

    atualização projeto advinha + exclusão de alguns

--- Lógica de Programação - JS e HTML/projeto-adivinha-wthinput/script.js
+++ Lógica de Programação - JS e HTML/projeto-adivinha-wthinput/script.js
@@ -1,13 +1,20 @@
-var segredo = 5;
+var segredos = [5,7,10,2];
 
 var input = document.querySelector("input");
 input.focus();
 
 function verifica() {
-    if (input.value == segredo ) {
-        alert("Você acertou");
-    } else {
-        alert("Você errou");
+    var achou = false;
+
+    for (i = 0; i < segredos.length; i++) {
+        if (input.value == segredos[i] ) {
+            alert("Você acertou");
+            achou = true;
+            break
+        } 
+    }
+    if (achou == false) {
+        alert ("Você errou!")
     }
 
     input.value = "";
